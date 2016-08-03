fe78ad0f8f14353bb97e10b78f4021aeb4aeab57
jrobinson17 <robinson039@gmail.com>
2016-08-03 21:41:18 +0000

    changed default permissions

--- userissues.sh
+++ userissues.sh
@@ -8,4 +8,4 @@ curl -k -v https://api.github.com/repos/docker/docker/issues/events >> txt.sh
 cat txt.sh |grep "login" >> naughty.txt
 rm -rf txt.sh
 
- curl --data "{"description": "Users with Outstanding Issues",  "public": true, "files": {   "troublemakers.txt": { "content": $(pwd)/naughty.txt } } }  "https://api.github.com/gists
+curl --data "{"description": "Users with Outstanding Issues",  "public": true, "files": {   "troublemakers.txt": { "content": $(pwd)/naughty.txt } } }  "https://api.github.com/gists
