b33df211634e26cac84ab28c63ba80452829fc6e
James <email>
2016-08-04 03:06:41 +0000

    changes 2

--- userissues.sh
+++ userissues.sh
@@ -5,5 +5,6 @@
 #curl -k -v https://api.github.com/repos/docker/docker/issues/events >> txt.sh
 #curl 'https://api.github.com/repos/docker/docker/issues/events' | grep login >> naughty.txt
 curl -s -X GET https://api.github.com/repos/docker/docker/issues/events |grep -Po '(?<="login": ")[^"]*' >> naughty.txt
-#curl -H "Content-Type: application/json" -d '{"description": "the description for this gist","public": true,"files": {"naughty.txt": {"content": "String file contents  '@naughty.txt'" }}}' https://api.github.com/gists |grep url
+#cxxxcurl -H "Content-Type: application/json" -d '{"description": "the description for this gist","public": true,"files": {"naughty.txt": {"content": "String file contents  '@naughty.txt'" }}}' https://api.github.com/gists |grep url
 
+# curl -H "Content-Type: application/json" -d '{"description": "the description for this gist","public": true,"files": {"naughty.txt": {"content": "String file contents  '@naughty.txt'" }}}' https://api.github.com/gists |grep url
